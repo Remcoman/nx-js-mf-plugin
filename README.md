0d4641019d6876ad3c8a0dcb99fb1c4029d5874a
Remco <info@remcokrams.nl>
2024-01-03 14:20:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nx-js-mf-plugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-js-mf-plugin` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-js-mf-plugin` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ jest.config.lib.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-js-mf-plugin',
+  preset: './jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nx-js-mf-plugin',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
